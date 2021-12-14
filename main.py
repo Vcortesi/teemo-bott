98628b7145a96f48500370d52abae1b4c9274063
Vincent Cortesi <vincentcortesi@gmail.com>
2021-12-14 16:43:56 -0600

    test

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Now you can start using Selenium
+#test
