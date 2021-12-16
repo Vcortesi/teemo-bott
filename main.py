c2293ca9eebf924bc16d05d55c92058fb1ec6ea9
Vincent Cortesi <vincentcortesi@gmail.com>
2021-12-16 16:16:59 -0600

    test

--- main.py
+++ main.py
@@ -8,38 +8,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from discord.ext import commands
 
-
-
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-path = "C://Users/Vince/AppData/Local/Programs/Python/chromedriver.exe" 
+PATH = "C://Users/Vince/AppData/Local/Programs/Python/chromedriver.exe"
+client = discord.Client() 
 script = " "
-client = discord.Client()
 
+# onReady def
 @client.event
 async def on_ready():
     print('Captain Teemo on duty!')
 
+# All of the commands the bot listens for
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+    # Shows the user a list of available commands
     if "!list" in message.content:
         await message.channel.send("Here is a list of current commands: ")
         await message.channel.send("!aram 'insert champion' -> grabs a build for aram\n!ranked 'insert champion' -> grabs a build for ranked\n!data 'summoner name' -> grabs data on summoner")
 
+    # Grabs a screenshot of aram builds for a specified champion
     if "!aram" in message.content:
         await message.channel.send(randomScript())
         champion = message.content[6:]
-       # chrome_options = Options()
-      #  chrome_options.add_argument("--start-maximized")
-       # chrome_options.add_argument("--headless") 
-        #driver=webdriver.Chrome(options=chrome_options,executable_path=path)
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get('https://app.mobalytics.gg/lol/champions/'+champion+'/aram-builds')
         driver.set_window_size(1920, 1080)
@@ -49,16 +47,13 @@ async def on_message(message):
         os.remove('screenshot.png')
         driver.quit()
 
+    # Grabs a screenshot of ranked builds for a specified champion
     if "!ranked" in message.content:
         await message.channel.send(randomScript())
         champion = message.content[8:]
-        #chrome_options = Options()
-        #chrome_options.add_argument("--start-maximized")
-        #chrome_options.add_argument("--headless") 
-        #driver=webdriver.Chrome(options=chrome_options,executable_path=path)
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get('https://probuildstats.com/champion/'+champion)
-        #driver.set_window_size(1920, 1080)
+        driver.set_window_size(1920, 1080)
         time.sleep(2)
         driver.find_element_by_class_name('champion-page_top-bar')
         driver.get_screenshot_as_file('screenshot.png')
@@ -66,13 +61,10 @@ async def on_message(message):
         os.remove('screenshot.png')
         driver.quit()
     
+    # Grabs a screenshot of counters for a specified champion
     if "!counter" in message.content:
         await message.channel.send(randomScript())
         champion = message.content[8:]
-        #chrome_options = Options()
-        #chrome_options.add_argument("--start-maximized")
-        #chrome_options.add_argument("--headless") 
-        #driver=webdriver.Chrome(options=chrome_options,executable_path=path)
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get('https://u.gg/lol/champions/'+champion+'/counter')
         driver.set_window_size(1920, 1080)
@@ -82,7 +74,8 @@ async def on_message(message):
         await message.channel.send(file=discord.File('screenshot.png'))
         os.remove('screenshot.png')
         driver.quit()
-        
+
+    # Uses call definitions to give the user data    
     if "!data" in message.content:
         await message.channel.send(randomScript())
         sumName = message.content[6:]
@@ -94,28 +87,30 @@ async def on_message(message):
         await message.channel.send("LP: " + str(responseJSON2[0]['leaguePoints']))
         await message.channel.send("Wins: " + str(responseJSON2[0]['wins']))
         await message.channel.send("Losses: " + str(responseJSON2[0]['losses']))
-        
-        
+
+# Randomly selects a prompt the bot will say on commands       
 def randomScript():
     randNum = random.randrange(1, 5)
     if(randNum == 1):
-        script = "Yes, sir!"
+        script = "'''Yes, sir!"
     if(randNum == 2):
-        script = "I'll scout ahead!"
+        script = "'''I'll scout ahead!"
     if(randNum == 3):
-        script = "Reporting in."
+        script = "'''Reporting in."
     if(randNum == 4):
-        script =  "Swiftly!"
+        script =  "'''Swiftly!"
     if(randNum == 5):
-        script = "Never underestimate the power of the Scout's code."
+        script = "'''Never underestimate the power of the Scout's code."
     return script
 
-def requestSummonerData(summonerName):
+# Grabs JSON from Riot's API
+async def requestSummonerData(summonerName):
     URL = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key="+os.environ.get('API')
     response = requests.get(URL)
     return response.json()
 
-def requestRankedData(ID):
+# Grabs JSON from Riot's API
+async def requestRankedData(ID):
     URL = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ID+"?api_key="+os.environ.get('API')
     response = requests.get(URL)
     return response.json()
